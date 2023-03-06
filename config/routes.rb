3e8d0a7b89a70bcd6706cee3e2bf48ae47e4bed1
James Kyan <james.kyan88@gmail.com>
2023-03-06 11:02:09 -0500

    Seeds and routes added

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :likes
+  resources :comments
+  resources :posts
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/users", to: "users#index"
 end
